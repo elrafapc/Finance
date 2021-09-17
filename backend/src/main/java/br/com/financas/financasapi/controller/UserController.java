22e8d5b706112c81af5bfdf197e72a3bfc2c11f8
Rafael <elrafapc@gmail.com>
2021-09-17 17:01:22 -0300

    POST e Delete endpoints created to User Class

--- backend/src/main/java/br/com/financas/financasapi/controller/UserController.java
+++ backend/src/main/java/br/com/financas/financasapi/controller/UserController.java
@@ -1,13 +1,13 @@
 package br.com.financas.financasapi.controller;
 
 import br.com.financas.financasapi.dto.UserDTO;
+import br.com.financas.financasapi.entities.User;
 import br.com.financas.financasapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,5 +21,16 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> findAll(){
         List<UserDTO> list = userService.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @PostMapping
+    public User insert(@Valid @RequestBody User user) {
+        return userService.insert(user);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
     }
 }
--- backend/src/main/java/br/com/financas/financasapi/service/UserService.java
+++ backend/src/main/java/br/com/financas/financasapi/service/UserService.java
@@ -4,8 +4,12 @@ import br.com.financas.financasapi.dto.UserDTO;
 import br.com.financas.financasapi.entities.User;
 import br.com.financas.financasapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,8 +22,25 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    @GetMapping()
     public List<UserDTO> findAll(){
         List<User> result = userRepository.findAll();
         return result.stream().map(item -> new UserDTO(item)).collect(Collectors.toList());
+    }
+
+    public User selectById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Error: Register was not found!"));
+    }
+
+    @PostMapping
+    public User insert(@Valid @RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable  Long id) {
+        User user = selectById(id);
+        userRepository.delete(user);
     }
 }
